1a68b5bd03a4512b24796e2089c29e25318724fc
mksyats <saddas1rambler@gmail.com>
2025-01-02 21:32:14 +0200

    chore: move utils folder inside scripts directory

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,5 @@
-import EventEmitter from '../utils/EventEmitter.js';
-import formatDateFromTimestamp from '../utils/formatDateFromTimestamp.js';
+import EventEmitter from './utils/EventEmitter.js';
+import formatDateFromTimestamp from './utils/formatDateFromTimestamp.js';
 
 const ee = new EventEmitter();
 
